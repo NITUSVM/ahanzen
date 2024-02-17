aeaf42f5c1871f3a7cde451184dee3f785df8862
NITUSVM <160360274+NITUSVM@users.noreply.github.com>
2024-02-17 16:14:33 -0500

    Add files via upload

---
+++ java/home.js
@@ -0,0 +1,17 @@
+alert ('hello');
+
+<button onclick="
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'Rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'Paper';
+    } else if (randomNumber >= 2 / 3 && < 1) {
+        computerMove = 'Scissors';
+    }
+
+    console.log (computerMove);
+">Rock</button>
