24b288c953d316a5225ce6ada1b10b61e5f7bf4f
NITUSVM <160360274+NITUSVM@users.noreply.github.com>
2024-02-26 18:23:47 -0500

    Update and rename home.js to index.js

---
+++ java/index.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menuList = document.getElementById('menuList');
+    menuList.style.display = (menuList.style.display === 'none' || menuList.style.display === '') ? 'block' : 'none';
+}
