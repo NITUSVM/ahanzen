93a0dfe275bdd2656b6e4438a327c64f0c8a6247
NITUSVM <160360274+NITUSVM@users.noreply.github.com>
2024-03-30 17:36:27 -0500

    Add files via upload

---
+++ javascript/cart.js
@@ -0,0 +1,12 @@
+// Función para abrir la página superpuesta
+    function openOverlay() {
+        document.getElementById('content').style.display = 'flex';
+      }
+  
+      // Función para cerrar la página superpuesta
+      function closeOverlay() {
+        document.getElementById('content').style.display = 'none';
+      }
+    function redirectToAlternative() {
+      window.location.href = "null.html";
+    }
